dd220f2ce8c610d6c9ffee689e38a107e7e46853
DavidHoskins <34613110+DavidHoskins@users.noreply.github.com>
2018-11-30 23:57:35 +0000

    Initial Commit

---
+++ ChooseYourOwnAdventure/Assets/Scripts/Player/PlayerInfoHandlers/PlayersStatsHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersStatsHandler : MonoBehaviour
+{
+    public int HealthPoints { get; set; }
+    public List<int> StatsValues = new List<int>();
+}
